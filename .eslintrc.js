6fe09e7856e5abd3f1d4622cdc3819be219130fb
Erica <erica@Ericade-iMac.local>
2021-02-27 23:29:03 +0800

    eslint standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es6': true,
-		'node': true
-	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 12,
-		'sourceType': 'module'
-	},
-	'plugins': [
-	],
-	'rules': {
-		'indent': ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		'quotes': ['error', 'single'],
-		'semi': ['error', 'never']
-	}
+  env: {
+    browser: true
+    // es2021: true
+  },
+  extends: [
+    'standard',
+    'prettier',
+    'plugin:eslint-comments/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 10
+  },
+  rules: {
+  }
 }
